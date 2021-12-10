0a6c2fbaba9ef8b61df32dfc3f87d0d2c465d60d
mxuexxmy <f9ab5014>
2021-12-10 23:07:36 +0800

    初始化

---
+++ src/item1/Point.java
@@ -0,0 +1,18 @@
+package item1;
+
+/**
+ * <h2> Point  </h2>
+ *
+ * @author mxuexxmy
+ * @date 2021/12/10 23:04
+ */
+public class Point {
+
+    double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
